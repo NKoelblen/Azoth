7a47813b8803bf7d3dc88dbe199dc4b5f72c4f73
Noémie Koelblen <noemie.koelblen@icloud.com>
2024-02-11 09:49:07 +0100

    Dashboard Widgets

---
+++ themes/azoth/inc/dashboard.php
@@ -0,0 +1,17 @@
+<?php
+/* Remove dashboard widgets */
+function wpdocs_remove_dashboard_widgets(){
+	remove_meta_box('dashboard_right_now', 'dashboard', 'normal');   // Right Now
+	remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal'); // Recent Comments
+	remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');  // Incoming Links
+	remove_meta_box('dashboard_plugins', 'dashboard', 'normal');   // Plugins
+	remove_meta_box('dashboard_quick_press', 'dashboard', 'side');  // Quick Press
+	remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');  // Recent Drafts
+	remove_meta_box('dashboard_primary', 'dashboard', 'side');   // WordPress blog
+	remove_meta_box('dashboard_secondary', 'dashboard', 'side');   // Other WordPress News
+	remove_meta_box('dashboard_activity', 'dashboard', 'normal');
+	remove_meta_box('wpseo-wincher-dashboard-overview', 'dashboard', 'normal');
+	remove_meta_box('wpseo-dashboard-overview', 'dashboard', 'normal');
+	remove_action('welcome_panel', 'wp_welcome_panel');	// use 'dashboard-network' as the second parameter to remove widgets from a network dashboard.
+}
+add_action('wp_dashboard_setup', 'wpdocs_remove_dashboard_widgets');
